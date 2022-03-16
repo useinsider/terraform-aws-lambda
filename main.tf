d2cdf4620117215e2c0679d895c952ed8c2bf521
serdarcanbigrivers <96129135+serdarcanbigrivers@users.noreply.github.com>
2022-03-16 09:01:57 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -276,5 +276,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.this[0].arn
     events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = var.bucket_prefix
   }
 }
